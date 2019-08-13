1bdaafd993a03521abe8d0cce3dc7078d4909960
Venkata Chalapathi <venkat.pv@gmail.com>
2019-08-13 13:17:47 +0530

    Initial commit

---
+++ app/src/main/java/com/venky/filedownloadingfromtheurl/ViewUploadsActivity.java
@@ -0,0 +1,108 @@
+package com.venky.filedownloadingfromtheurl;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewUploadsActivity extends AppCompatActivity {
+
+    //the listview
+    ListView listView;
+
+    //database reference to get uploads data
+    DatabaseReference mDatabaseReference;
+
+    //list to store uploads data
+    List<Upload> uploadList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_uploads);
+
+        uploadList = new ArrayList<>();
+        listView = (ListView) findViewById(R.id.listView);
+
+
+        //adding a clicklistener on listview
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //getting the upload
+                Upload upload = uploadList.get(i);
+
+                Toast.makeText(ViewUploadsActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+                Log.i("url-->",""+uploadList.get(i).getName());
+                //Opening the upload file in browser using the upload url
+               /* Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(upload.getUrl()));
+                startActivity(intent);*/
+               downloadFile(ViewUploadsActivity.this,uploadList.get(i).getName(),
+                       ".pdf","SIETK/",upload.getUrl());
+            }
+        });
+
+
+        //getting the database reference
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference(Constants.DATABASE_PATH_UPLOADS);
+
+        //retrieving upload data from firebase database
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    String s = postSnapshot.child("url").getValue(String.class);
+                    Toast.makeText(ViewUploadsActivity.this, ""+s, Toast.LENGTH_SHORT).show();
+                    Upload upload = postSnapshot.getValue(Upload.class);
+                    uploadList.add(upload);
+                }
+
+                String[] uploads = new String[uploadList.size()];
+
+                for (int i = 0; i < uploads.length; i++) {
+                    uploads[i] = uploadList.get(i).getName();
+                }
+
+                //displaying it to list
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, uploads);
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+    public long downloadFile(Context context, String fileName, String fileExtension, String destinationDirectory, String url) {
+
+
+        DownloadManager downloadmanager = (DownloadManager) context.
+                getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName + fileExtension);
+
+        return downloadmanager.enqueue(request);
+    }
+}
